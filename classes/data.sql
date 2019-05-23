919560b2ac3ca4ae261d6412f0c04c4454dc4cc6
swetha <swethamachiraju893@gmail.com>
2019-05-23 15:45:32 -0400

    first commit

---
+++ classes/data.sql
@@ -0,0 +1,5 @@
+insert into Tacos(id, taco_name, price) values (1001,'veggie taco', 2.50);
+insert into Tacos(id, taco_name, price) values (1002,'chicken taco', 3.00);
+insert into Tacos(id, taco_name, price) values (1003,'beef taco', 3.00);
+insert into Tacos(id, taco_name, price) values (1004,'chorizo taco', 3.50);
+insert into order_details(id, price, quantity ) values (100, 7.00,4);
